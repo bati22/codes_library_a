b2cf8c1376161d6ec7f4180a12fe6e31d58a43f0
bati22 <40314986+bati22@users.noreply.github.com>
2019-10-30 21:26:38 +0100

    Update the_two_routes.cpp

--- the_two_routes.cpp
+++ the_two_routes.cpp
@@ -1,24 +1,7 @@
 // K2. The Two Routes
 //http://codeforces.com/group/iKIf4DVs3E/contest/206748/problem/K2
-
-
-/*
-Opis
-Na wej?ciu dostaj? pary miast, kt?re s? po??czone kolej?. Reszta miast jest po??czona drog? samochodow?. Podr? zaczyna si? w mie?cie 1, a ko?czy w mie?cie n. Z warunk?w zadania wynika, ?e pojazdy nie mog? by? w tym samym czasie, w tym samym mie?cie (za wyj?tkiem miasta n). Wykonuj? bfsa dla dowolnego z pojazd?w (przy czym licz? r?wnie? czas podr?y; je?li znajduj? wierzcho?ek n, przerwyam funkcj? i zapisuj? czas jako czas pojazdu). Tworz? dodatkow? tablic? "czas?w", w kt?rej umieszczam odwiedzone przez pierwszy pojazd miasta, w zale?no?ci od czasu (za wyj?tkiem miasta n), np. po 4 godzinach pierwszy pojazd by? w mie?cie 3, wi?c:
-bool tablica_czas[405];
-tablica_czas[4] = 3;
-Wykonuj? bfsa na drugim poje?dzie, ale tym razem uwzgl?dniam tablic?  "czas?w", tzn. nie mog? jecha? do miasta w kt?rym w tym samym czasie jest pierwszy pojazd, np. je?eli czas wynosi 7, to nie mog? jecha? do miasta tablica_czas[7].
-Je?eli w pierwszym lub drugim bfsie nie dotar?em do wierzcho?ka n, to wyrzucam -1, w przeciwnym wypadk?w wyrzucam wi?kszy czas.
-
-
-Dow?d
-Droga samochododowa jest mi?dzy tymi miastami, kt?re nie zosta?y po??czone kolej?. Bfs szuka najkr?tszej mo?liwej drogi z punktu A do punktu B, wi?c wyliczy najkr?tszy mo?liwy czas dla pojazd?w. Uwzgl?dni?em , ?e w pojazdy nie mog? pojecha? do tego samego miasta, w tym samym czasie.
-
-
-
-
-*/
-
+//BFS
+//O(n)
 
 #include <iostream>
 #include <vector>
